cc9d4ca94a992b735d837a5e90b1219abd9120ff
azrafe7 <azrafe7@gmail.com>
2018-11-24 22:43:33 +0100

    [format] Use Sys.programPath()

--- src/FormatAbstractPixelsDemo.hx
+++ src/FormatAbstractPixelsDemo.hx
@@ -16,9 +16,9 @@ enum DataWrapper {
 }
 
 class FormatAbstractPixelsDemo {
-	
+
 	var startTime:Float = 0.0;
-	
+
 	public static function main(): Void {
 		new FormatAbstractPixelsDemo();
 	}
@@ -27,17 +27,17 @@ class FormatAbstractPixelsDemo {
 		for (id in Resource.listNames()) {
 			var res = Resource.getBytes(id);
 			var bytesInput = new BytesInput(res);
-			
+
 			trace('[ testing $id ]');
-			
+
 			test(bytesInput, id);
 		}
 	}
-	
+
 	public function test(input:BytesInput, id:String):Void {
-		
+
 		var ext = id.substr(-3).toLowerCase();
-		
+
 		// read input into data using format tools
 		startTime = Timer.stamp();
 		var dataWrapper = switch (ext) {
@@ -45,27 +45,27 @@ class FormatAbstractPixelsDemo {
 				var pngReader = new format.png.Reader(input);
 				var data = pngReader.read();
 				PNG(data);
-				
+
 			case "bmp":
 				var bmpReader = new format.bmp.Reader(input);
 				var data = bmpReader.read();
 				BMP(data);
-				
+
 			case "gif":
 				var gifReader = new format.gif.Reader(input);
 				var data = gifReader.read();
 				GIF(data);
-				
+
 			default:
 				Invalid;
 		}
 		trace('read        ${Timer.stamp() - startTime}');
-		
+
 		if (dataWrapper == Invalid) {
 			trace("ERROR: Invalid data.\n");
 			return;
 		}
-		
+
 		// load bitmapData into pixels abstract
 		var pixels:Pixels = null;
 		startTime = Timer.stamp();
@@ -76,42 +76,38 @@ class FormatAbstractPixelsDemo {
 			default: null;
 		}
 		trace('load        ${Timer.stamp() - startTime}');
-		
+
 		// generate random points
 		var points = [];
 		var NUM_POINTS = 10000;
 		for (i in 0...NUM_POINTS) points.push( { x: Std.random(pixels.width), y: Std.random(pixels.height) } );
-		
+
 		// read random points
 		startTime = Timer.stamp();
 		for (i in 0...NUM_POINTS) {
 			var color = pixels.getPixel32(points[i].x, points[i].y);
 		}
 		trace('get         ${Timer.stamp() - startTime}');
-		
+
 		// add random red points
 		startTime = Timer.stamp();
 		for (i in 0...NUM_POINTS) {
 			pixels.setPixel32(points[i].x, points[i].y, 0xFFFF0000);
 		}
-		// if this green line doesn't go _exactly_ from top-left to bottom-right, 
+		// if this green line doesn't go _exactly_ from top-left to bottom-right,
 		// then there's something wrong with the Pixels impl.
 		Bresenham.line(pixels, 0, 0, pixels.width - 1, pixels.height - 1, 0x00FF00);
 		trace('set         ${Timer.stamp() - startTime}');
-		
+
 		// trace info
 		trace("pixels      " + pixels.width, pixels.height, pixels.count, StringTools.hex(pixels.getPixel32(0, 0)));
-		
+
 		writeModifiedPNG(pixels, id);
 		writeModifiedBMP(pixels, id);
 	}
-	
+
 	public function writeModifiedPNG(pixels:Pixels, fileName:String) {
-	#if neko	
-		var dir = Path.directory(neko.vm.Module.local().name);
-	#else
-		var dir = Path.directory(Sys.executablePath());
-	#end
+		var dir = Path.directory(Sys.programPath());
 		var outputFileName = "out_" + fileName + ".png";
 		var file = sys.io.File.write(Path.join([dir, outputFileName]), true);
 		var pngWriter = new format.png.Writer(file);
@@ -122,13 +118,9 @@ class FormatAbstractPixelsDemo {
 		pngWriter.write(pngData);
 		trace("written to '" + outputFileName + "'\n");
 	}
-	
+
 	public function writeModifiedBMP(pixels:Pixels, fileName:String) {
-	#if neko	
-		var dir = Path.directory(neko.vm.Module.local().name);
-	#else
-		var dir = Path.directory(Sys.executablePath());
-	#end
+		var dir = Path.directory(Sys.programPath());
 		var outputFileName = "out_" + fileName + ".bmp";
 		var file = sys.io.File.write(Path.join([dir, outputFileName]), true);
 		var bmpWriter = new format.bmp.Writer(file);
